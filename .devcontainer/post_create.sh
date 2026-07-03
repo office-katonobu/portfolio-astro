8129bbbcfcf8ebebca9cdcfa64e6fa04b8a75444
Nobuo Kato <katonobu@office-katonobu.com>
2026-07-03 07:54:50 +0000

    https://github.com/odhyp/astro-sidey.git からコード引っ張ってきた。

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,17 @@
+# Node.js と npm が必要
+sudo apt-get update
+sudo apt-get install -y nodejs npm
+
+# mkdir tmp
+# cd tmp
+# git clone --depth 1 --branch main https://github.com/odhyp/astro-sidey.git
+# cp -r tmp/* ..
+# rm -rf tmp
+
+npm install
+
+echo "versions"
+node --version
+npm --version
+
+echo "post create finished"
---
+++ .devcontainer/post_start.sh
@@ -0,0 +1,2 @@
+
+echo "post start finished"
